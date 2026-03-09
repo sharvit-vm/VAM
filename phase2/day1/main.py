2bb1f45f9506b427ce0a6961bfbe80b6f0e6268d
Sharvit Nilesh Kashikar <sharvit.kashikar@valuemomentum.com>
2026-03-09 12:38:16 +0530

    day1 phase 2

---
+++ phase2/day1/main.py
@@ -0,0 +1,27 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+load_dotenv()
+
+#settings 
+temperature = 0.8
+max_tokens=400
+
+print("choose model")
+print("1: gemini flash")
+print("2: gemini pro")
+choice = input("enter choice:")
+
+if choice == "1":
+    model = ChatGoogleGenerativeAI(model= "gemini-2.5-flash-lite", temperature=temperature, max_output_tokens=max_tokens)
+
+elif choice =="2":
+    model = ChatGoogleGenerativeAI(model = "gemini-2.5-pro",temperature=temperature, max_output_tokens=max_tokens)
+
+else:
+    print("Invalid choice")
+    exit()
+
+prompt = input("\nEnter your prompt: ")
+response = model.invoke(prompt)
+print("\nResponse:\n")
+print(response.content)
